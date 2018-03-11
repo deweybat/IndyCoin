98bd5933ef0d8e50a2da978c1ee2ad51aaea503a
Dat Bui <dbdatbui@gmail.com>
2018-03-11 00:07:29 -0500

    Fixed Flask imports

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,11 @@
 import hashlib
 import json
+from textwrap import dedent
 from time import time
+from urllib.parse import urlparse
 from uuid import uuid4
 
+import requests
 from flask import Flask, jsonify, request
 
 class Blockchain(object):
@@ -38,7 +41,7 @@ class Blockchain(object):
 			'recipient': recipient,
 			'amount': amount,
 			})
-		return self.last_block[index] + 1
+		return self.last_block['index'] + 1
 
 	@staticmethod
 	def hash(block):
@@ -52,7 +55,7 @@ class Blockchain(object):
 
 	def proof_of_work(self, last_proof):
 		proof = 0
-		while self.valid_proof(last_proof, proof) is False:
+		while self.valid_proof(last_proof,proof) is False:
 			proof += 1
 
 		return proof
@@ -64,7 +67,7 @@ class Blockchain(object):
 		return guess_hash[:4] == "0000"
 
 #Instantiate the Node
-app = flas(__name__)
+app = Flask(__name__)
 
 node_identifier = str(uuid4()).replace('-', '')
 
@@ -76,13 +79,23 @@ def mine():
 	last_proof = last_block['proof']
 	proof = blockchain.proof_of_work(last_proof)
 
-	blockchain.new_transaction {
+	blockchain.new_transaction (
 		sender="0",
 		recipient=node_identifier,
 		amount=1,
-	}
+	)	
 
-previous_hash = blockchain.hash(last_block)
+	previous_hash = blockchain.hash(last_block)
+	block = blockchain.new_block(proof, previous_hash)
+
+	response = {
+		'message': "New Block Forged",
+		'index': block['index'],
+		'transactions': block['transactions'],
+		'proof': block['proof'],
+		'previous_hash': block['previous_hash'],
+	}
+	return jsonify(response), 200
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
 	values = request.get_json()
@@ -93,7 +106,7 @@ def new_transaction():
 
 	index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
 
-	response = {'message': f'Transaction will be added to Block (index)'}
+	response = {'message': 'Transaction will be added to Block (index)'}
 	return jsonify(response), 201
 
 @app.route('/chain', methods=['GET'])
@@ -105,4 +118,4 @@ def full_chain():
 	return jsonify(response), 200
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+	app.run(host='127.0.0.1', port=5000)
